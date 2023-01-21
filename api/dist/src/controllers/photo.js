6055012337a3d1b31e6bc8acc6790a5ea7c0e6f5
Fatih Şen <fatihydrm@outlook.com>
2023-01-22 01:03:27 +0300

    delete: typescript remainders

--- api/dist/src/controllers/photo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.register = exports.login = void 0;
-const login = async () => { };
-exports.login = login;
-const register = async () => { };
-exports.register = register;
--- api/src/routes/photo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var users_1 = require("../controllers/users");
-var router = express_1["default"].Router();
-router.get("/", users_1.login);
-router.get("/", users_1.register);
-exports["default"] = router;
