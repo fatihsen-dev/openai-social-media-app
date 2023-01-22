a9c7e4f5a6c4c2ff11ad8eccc8bcd338e39046c2
Fatih Şen <fatihydrm@outlook.com>
2023-01-22 21:32:26 +0300

    Edit: client pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,40 @@
+import { useDispatch, useSelector } from "react-redux/es/exports";
 import Logo from "./components/Logo";
+import { Dispatch, RootState } from "./store";
+import {} from "./store/auth/authSlice";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Explore from "./pages/Explore";
+import Auth from "./pages/Auth";
+import NotFound from "./pages/NotFound";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function App() {
+   const dispath = useDispatch<Dispatch>();
+   const { status } = useSelector((store: RootState) => store.auth);
+
    return (
-      <div className='h-full container grid place-items-center'>
-         <Logo color='#222' size={300} icon='red' />
+      <div className='bg-dark h-full text-white'>
+         {status !== null ? (
+            <div className='h-full container bg-dark border'>
+               <Routes>
+                  <Route index element={<Home />} />
+                  {status ? <Route path='/profile' element={<Profile />} /> : null}
+                  {status === false ? <Route path='/auth' element={<Auth />} /> : null}
+                  <Route path='/explore' element={<Explore />} />
+                  <Route path='*' element={<NotFound />} />
+               </Routes>
+            </div>
+         ) : (
+            <div className='h-full flex justify-center items-center'>
+               <Player
+                  autoplay
+                  loop
+                  src='https://assets7.lottiefiles.com/packages/lf20_3J0owIXaM6.json'
+                  style={{ height: "400px", width: "400px" }}></Player>
+            </div>
+         )}
       </div>
    );
 }
