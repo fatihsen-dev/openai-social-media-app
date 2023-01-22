003dfab9d9fb422dd28dab5fce74bd6282c39ecd
Fatih Şen <fatihydrm@outlook.com>
2023-01-22 21:29:46 +0300

    edit: user router

--- api/index.ts
+++ api/index.ts
@@ -19,7 +19,7 @@ app.listen(process.env.PORT || 5000, () => {
    console.log(`Server listen ${process.env.PORT || 5000}`);
 });
 
-app.use("/users", userRouter);
+app.use("/user", userRouter);
 app.use("/photo", photoRouter);
 
 (async () => {
