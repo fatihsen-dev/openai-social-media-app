456318ba176054ef4e7abf6331a142d0a6f856d5
Fatih Şen <fatihydrm@outlook.com>
2023-01-26 18:17:56 +0300

    edit: update auth page

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,3 +1,43 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
+import { Toast } from "../components/Toast";
+
 export default function Auth() {
-   return <div>Auth</div>;
+   const location = useLocation();
+   const [auth, setAuth] = useState<boolean>(true);
+   const [btnSubmit, setBtnSubmit] = useState<boolean>(true);
+
+   useEffect(() => {
+      setAuth(location.state.auth);
+   }, []);
+
+   const onSubmit = (e: any) => {
+      e.preventDefault();
+
+      const { username, mail, password } = e.target;
+      if (username.value.length === 0 && !btnSubmit) {
+         Toast({ message: "Username cannot be blank" });
+      } else {
+         if (!btnSubmit) {
+            if (username.value.length < 4 || username.value.length > 20) {
+               Toast({ message: "Username must be greater than 4 and less than 20" });
+            }
+         } else {
+            console.log("dssadsa");
+         }
+      }
+   };
+
+   return (
+      <div className='h-full flex justify-center items-center flex-col'>
+         <AuthForm
+            auth={auth}
+            onSubmit={onSubmit}
+            setAuth={setAuth}
+            btnSubmit={btnSubmit}
+            setBtnSubmit={setBtnSubmit}
+         />
+      </div>
+   );
 }
