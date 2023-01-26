4827e8c297c37e79b7ed69bec62515a573a77f74
Fatih Şen <fatihydrm@outlook.com>
2023-01-26 18:39:09 +0300

    create: user control request

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, register } from "../controllers/users";
+import { login, register, control } from "../controllers/users";
 
 const router = express.Router();
 
 router.post("/login", login);
+router.post("/control", control);
 router.post("/register", register);
 
 export default router;
