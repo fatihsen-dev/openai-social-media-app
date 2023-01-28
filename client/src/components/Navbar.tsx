1801dab5020646ba513dff0529672895c2019401
Fatih Şen <fatihydrm@outlook.com>
2023-01-28 21:13:59 +0300

    edit: navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,13 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import Logo from "./Logo";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 export default function Navbar() {
+   const navigate = useNavigate();
+
+   const { user, status } = useSelector((state: RootState) => state.auth);
+
    return (
       <div className='w-full border-b border-light p-3 pb-3.5 px-4 flex items-center justify-between'>
          <NavLink className='text-lg text-light' to='/'>
@@ -19,20 +25,28 @@ export default function Navbar() {
                Explore
             </NavLink>
          </ul>
-         <ul className='flex gap-3 font-medium'>
-            <NavLink
-               state={{ auth: true }}
-               className='bg-orange text-light px-6 py-0.5 rounded-sm'
-               to='/auth'>
-               Login
-            </NavLink>
-            <NavLink
-               state={{ auth: false }}
-               className='bg-darkBlue text-light px-6 py-0.5 rounded-sm'
-               to='/auth'>
-               Register
-            </NavLink>
-         </ul>
+         {status ? (
+            <button
+               onClick={() => navigate("/profile")}
+               className='text-lg font-medium cursor-pointer'>
+               {user.username}
+            </button>
+         ) : (
+            <ul className='flex gap-3 font-medium'>
+               <NavLink
+                  state={{ auth: true }}
+                  className='bg-orange text-light px-6 py-0.5 rounded-sm'
+                  to='/auth'>
+                  Login
+               </NavLink>
+               <NavLink
+                  state={{ auth: false }}
+                  className='bg-darkBlue text-light px-6 py-0.5 rounded-sm'
+                  to='/auth'>
+                  Register
+               </NavLink>
+            </ul>
+         )}
       </div>
    );
 }
