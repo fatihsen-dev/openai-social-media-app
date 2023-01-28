4c5c832b4f1506f0ea1e9db54511a606de887a53
Fatih Şen <fatihydrm@outlook.com>
2023-01-28 21:13:52 +0300

    edit: user control router

--- client/src/axios/index.ts
+++ client/src/axios/index.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
-
 const HTTP = axios.create({
-   baseURL: import.meta.env.VITE_SERVER_URL,
+   baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const loginRequest = async (data: { email: string; password: string }) =>
    await HTTP.post("/user/login", data);
 
-export const controlRequest = async (token: string) =>
-   await HTTP.post("/user/login", token);
+export const controlRequest = async (data: { token: string; _id: string }) =>
+   await HTTP.post("/user/control", data);
 
 export const registerRequest = async (data: {
    username: string;
