24aeb9a2bbea52df34349dab281958bfc4d69d0b
Fatih Şen <fatihydrm@outlook.com>
2023-01-28 21:14:25 +0300

    edit: login & register page

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,43 +1,19 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import AuthForm from "../components/AuthForm";
-import { Toast } from "../components/Toast";
+import LoginForm from "../components/LoginForm";
+import RegisterForm from "../components/RegisterForm";
 
 export default function Auth() {
    const location = useLocation();
    const [auth, setAuth] = useState<boolean>(true);
-   const [btnSubmit, setBtnSubmit] = useState<boolean>(true);
 
    useEffect(() => {
       setAuth(location.state.auth);
    }, []);
 
-   const onSubmit = (e: any) => {
-      e.preventDefault();
-
-      const { username, mail, password } = e.target;
-      if (username.value.length === 0 && !btnSubmit) {
-         Toast({ message: "Username cannot be blank" });
-      } else {
-         if (!btnSubmit) {
-            if (username.value.length < 4 || username.value.length > 20) {
-               Toast({ message: "Username must be greater than 4 and less than 20" });
-            }
-         } else {
-            console.log("dssadsa");
-         }
-      }
-   };
-
    return (
       <div className='h-full flex justify-center items-center flex-col'>
-         <AuthForm
-            auth={auth}
-            onSubmit={onSubmit}
-            setAuth={setAuth}
-            btnSubmit={btnSubmit}
-            setBtnSubmit={setBtnSubmit}
-         />
+         {auth ? <LoginForm setAuth={setAuth} /> : <RegisterForm setAuth={setAuth} />}
       </div>
    );
 }
