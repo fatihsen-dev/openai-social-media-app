2779f7a4f08ad7007028d9b642d66116a02240c2
Fatih Şen <fatihydrm@outlook.com>
2023-01-28 21:13:08 +0300

    edit: user control

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux/es/exports";
 import { Dispatch, RootState } from "./store";
-import { Logout } from "./store/auth/authSlice";
-import { Routes, Route } from "react-router-dom";
+import { Login, Logout } from "./store/auth/authSlice";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Explore from "./pages/Explore";
@@ -10,15 +10,40 @@ import NotFound from "./pages/NotFound";
 import { Player } from "@lottiefiles/react-lottie-player";
 import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
+import { controlRequest } from "./axios";
+import { Toast } from "./components/Toast";
 
 export default function App() {
-   const dispath = useDispatch<Dispatch>();
+   const dispatch = useDispatch<Dispatch>();
+   const navigate = useNavigate();
    const { status } = useSelector((store: RootState) => store.auth);
 
    useEffect(() => {
       if (localStorage.getItem("token")) {
+         (async () => {
+            try {
+               const { token, _id } = JSON.parse(`${localStorage.getItem("token")}`);
+               const response = await controlRequest({ token, _id });
+               setTimeout(() => {
+                  dispatch(Login({ user: response.data, status: true }));
+               }, 500);
+               localStorage.setItem(
+                  "token",
+                  JSON.stringify({ _id: response.data._id, token: response.data.token })
+               );
+               navigate("/");
+            } catch (error: any) {
+               Toast({ message: error.response.data.message });
+               setTimeout(() => {
+                  dispatch(Logout());
+               }, 500);
+               navigate("/");
+            }
+         })();
       } else {
-         dispath(Logout());
+         setTimeout(() => {
+            dispatch(Logout());
+         }, 500);
       }
    }, []);
 
