3b10f589c5821d8dfbbd44c73f5480c14db7ee5f
Fatih Şen <fatihydrm@outlook.com>
2023-02-01 20:07:48 +0300

    add: home page animation

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,22 +1,21 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
 import { Helmet } from "react-helmet";
+import HomeSlider from "../components/HomeSlider";
+import Logo from "../components/Logo";
 
 export default function Home() {
-   const [page, setPage] = useState("login");
-   const [data, setData] = useState({
-      title: "Login",
-      button: "Login",
-   });
-
    return (
       <>
          <Helmet>
             <title>Home</title>
          </Helmet>
-         <div className='text-whit flex flex-col items-start'>
+         <div className='text-whit h-full flex flex-col items-start'>
             <Navbar />
-            <div className='flex-1 bg-red-500'></div>
+            <div className='flex-1 flex flex-col gap-12 w-full justify-center items-center'>
+               <Logo className='2xl:w-[260px] sm:w-[260px] w-[200px]' size={260} />
+               <HomeSlider />
+            </div>
          </div>
       </>
    );
