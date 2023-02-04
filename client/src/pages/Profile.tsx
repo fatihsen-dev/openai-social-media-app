05affd3882daffa8a11d1c22c2fb48ac69c6f705
Fatih Şen <fatihydrm@outlook.com>
2023-02-04 21:22:35 +0300

    edit: update profile bage

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -24,9 +24,9 @@ export default function Profile() {
             <Navbar />
             <div className='flex flex-1 p-10 gap-10 overflow-auto'>
                <div className='relative w-60'>
-                  <div className='bg-dark2 fixed px-8 h-[290px] rounded-sm flex justify-center flex-col gap-5'>
+                  <div className='bg-dark2 fixed px-10 h-[290px] rounded-sm flex justify-center flex-col gap-5'>
                      <img
-                        className='w-44 rounded-full'
+                        className='w-36 rounded-full'
                         src={`./images/home/img-${randomAvatar}.png`}
                         alt=''
                      />
@@ -39,7 +39,27 @@ export default function Profile() {
                   </div>
                </div>
                <div className='flex-1 flex flex-col gap-10'>
-                  <div className='h-[290px] bg-red-500 rounded-sm'></div>
+                  <div className='h-[290px] bg-dark2 rounded-sm p-6 flex items-center gap-6'>
+                     <img
+                        className='w-60 rounded-sm'
+                        src={`./images/home/img-6.png`}
+                        alt=''
+                     />
+                     <form className='h-full flex flex-col flex-1 gap-5'>
+                        <textarea
+                           placeholder='Prompt'
+                           className='w-full rounded-sm resize-none flex-1 p-2 py-1 bg-[#1D1D1D] border border-dark'></textarea>
+                        <div className='w-full flex gap-5'>
+                           <span className='bg-darkBlue rounded-sm hover:bg-darkBlue/80 transition-colors flex-1 text-center py-2 cursor-pointer'>
+                              Create
+                           </span>
+                           <button className='bg-orange rounded-sm hover:bg-orange/80 transition-colors flex-1 py-2'>
+                              Share
+                           </button>
+                        </div>
+                     </form>
+                  </div>
+                  <div className='h-[290px] flex-1'></div>
                </div>
             </div>
          </div>
