273b2be40630aef8cebf96fe9d5f2ce718f72fe9
Fatih Şen <fatihydrm@outlook.com>
2023-02-11 17:01:35 +0300

    add: create redux imageSlice types

---
+++ client/src/store/images/types.ts
@@ -0,0 +1,27 @@
+export type ImageType = {
+   images: [
+      {
+         _id: string;
+         prompt: string;
+         image: string;
+         owner: {
+            email: string;
+            username: string;
+            _id: string;
+         };
+         created: string;
+      }
+   ];
+};
+
+export type OneImgType = {
+   _id: string;
+   prompt: string;
+   image: string;
+   owner: {
+      email: string;
+      username: string;
+      _id: string;
+   };
+   created: string;
+};
