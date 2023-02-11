dbc6f4adf459fc07477f199ff343b6c8d2f78895
Fatih Şen <fatihydrm@outlook.com>
2023-02-11 16:56:40 +0300

    edit: configure openai config and add express public folder

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,4 @@
+import { OpenAIApi } from "openai";
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
@@ -7,7 +8,9 @@ const app = express();
 import mongoose from "mongoose";
 import userRouter from "./src/routes/users";
 import photoRouter from "./src/routes/photo";
+import { Configuration } from "openai/dist/configuration";
 
+app.use(express.static("src/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(helmet());
 app.use(express.static("src/public"));
@@ -18,6 +21,12 @@ app.use(cors());
 app.listen(process.env.PORT || 5000, () => {
    console.log(`Server listen ${process.env.PORT || 5000}`);
 });
+
+const configuration = new Configuration({
+   apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const openai = new OpenAIApi(configuration);
 
 app.use("/user", userRouter);
 app.use("/photo", photoRouter);
