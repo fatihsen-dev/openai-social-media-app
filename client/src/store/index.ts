2eb032ae3a848001317a4ca225d272024dc6d2aa
Fatih Şen <fatihydrm@outlook.com>
2023-02-11 17:00:10 +0300

    add: redux new imageSlice

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,11 @@
+import imageSlice from "./images/imageSlice";
 import authSlice from "./auth/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
    reducer: {
       auth: authSlice,
+      images: imageSlice,
    },
 });
 
