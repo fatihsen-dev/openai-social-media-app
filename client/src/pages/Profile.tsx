cc1ce4ecec94d5b6c2d94106e5e9ba8ca3b0e871
Fatih Şen <fatihydrm@outlook.com>
2023-02-11 16:59:31 +0300

    edit: profile page

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,19 +1,58 @@
 import { Helmet } from "react-helmet";
 import Navbar from "../components/Navbar";
-import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import { useState, useEffect } from "react";
+import { BsImage } from "react-icons/bs";
+import { createImage, updateImageState } from "../axios";
 
 export default function Profile() {
+   const [image, setImage] = useState<string>("");
+   const [imageId, setImageId] = useState<string>("");
+   const [wait, setWait] = useState<boolean>(false);
+
    const [randomAvatar, setRandomAvatar] = useState<number>();
-   const navigate = useNavigate();
-   const dispatch = useDispatch();
    const { user } = useSelector((state: RootState) => state.auth);
 
    useEffect(() => {
       setRandomAvatar(Math.floor(Math.random() * 30) + 1);
    }, []);
+
+   const createImageFormHandle = async (e: any) => {
+      e.preventDefault();
+
+      setWait(true);
+
+      if (e.target.prompt.value.length > 0) {
+         try {
+            const response = await createImage({
+               owner: user._id,
+               prompt: e.target.prompt.value,
+            });
+            setImage(response.data.image);
+            setImageId(response.data._id);
+            setWait(false);
+         } catch (err) {
+            setWait(false);
+            console.log(err);
+         }
+      } else {
+         alert("oluşturulamadı");
+      }
+   };
+
+   const shareHandle = async () => {
+      if (imageId) {
+         try {
+            await updateImageState({ _id: imageId });
+            setImageId("");
+         } catch (err) {
+            console.log(err);
+         }
+      } else {
+         alert("Lütfen önce resim oluşturun");
+      }
+   };
 
    return (
       <div className='h-full flex'>
@@ -40,22 +79,36 @@ export default function Profile() {
                </div>
                <div className='flex-1 flex flex-col gap-10'>
                   <div className='h-[290px] bg-dark2 rounded-sm p-6 flex items-center gap-6'>
-                     <img
-                        className='w-60 rounded-sm'
-                        src={`./images/home/img-6.png`}
-                        alt=''
-                     />
-                     <form className='h-full flex flex-col flex-1 gap-5'>
+                     <div className='w-60 h-60 bg-[#1D1D1D] rounded-sm flex justify-center items-center'>
+                        {image ? (
+                           <img
+                              className='w-full h-full object-cover rounded-sm'
+                              src={`${import.meta.env.VITE_API_URL}/${image}`}
+                              alt=''
+                           />
+                        ) : (
+                           <BsImage className='text-4xl text-dark2' />
+                        )}
+                     </div>
+                     <form
+                        onSubmit={createImageFormHandle}
+                        className='h-full flex flex-col flex-1 gap-5'>
                         <textarea
                            placeholder='Prompt'
+                           name='prompt'
                            className='w-full rounded-sm resize-none flex-1 p-2 py-1 bg-[#1D1D1D] border border-dark'></textarea>
                         <div className='w-full flex gap-5'>
-                           <span className='bg-darkBlue rounded-sm hover:bg-darkBlue/80 transition-colors flex-1 text-center py-2 cursor-pointer'>
-                              Create
-                           </span>
-                           <button className='bg-orange rounded-sm hover:bg-orange/80 transition-colors flex-1 py-2'>
-                              Share
+                           <button
+                              disabled={wait}
+                              className='disabled:bg-orange/50 disabled:text-white/50 bg-orange rounded-sm hover:bg-orange/80 transition-colors flex-1 py-2 flex items-center justify-center gap-4'>
+                              {wait && <span className='loader'></span>}
+                              <span>Create</span>
                            </button>
+                           <span
+                              onClick={shareHandle}
+                              className='bg-darkBlue rounded-sm hover:bg-darkBlue/80 transition-colors flex-1 text-center py-2 cursor-pointer'>
+                              Share
+                           </span>
                         </div>
                      </form>
                   </div>
