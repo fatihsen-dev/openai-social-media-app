62237153c79547b01668ab538a5e0589a52074f5
Fatih Şen <fatihydrm@outlook.com>
2023-02-11 16:58:20 +0300

    add: new routers

--- api/src/routes/photo.ts
+++ api/src/routes/photo.ts
@@ -1,9 +1,10 @@
-import express from "express";
-import { login, register } from "../controllers/users";
+import express, { NextFunction, Request, Response } from "express";
+import { getAllPosts, postCreate, postUpdate } from "../controllers/photo";
 
 const router = express.Router();
 
-router.get("/", login);
-router.get("/", register);
+router.get("/", getAllPosts);
+router.post("/create", postCreate);
+router.post("/status", postUpdate);
 
 export default router;
