1ad98ecd69f39f5bdaccfabcdff905de738b721c
Fatih Şen <fatihydrm@outlook.com>
2023-02-13 21:13:13 +0300

    add: new user photo request

--- api/src/routes/photo.ts
+++ api/src/routes/photo.ts
@@ -1,10 +1,11 @@
-import express, { NextFunction, Request, Response } from "express";
-import { getAllPosts, postCreate, postUpdate } from "../controllers/photo";
+import express from "express";
+import { getAllPosts, postCreate, postUpdate, getUserImages } from "../controllers/photo";
 
 const router = express.Router();
 
 router.get("/", getAllPosts);
 router.post("/create", postCreate);
 router.post("/status", postUpdate);
+router.get("/userimages", getUserImages);
 
 export default router;
